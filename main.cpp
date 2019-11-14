49e6885a95dbc95e5b1d15bc1982958fde2a0ec5
Tristan Stenner <ttstenner@gmail.com>
2019-11-14 09:10:13 +0100

    Update boilerplate code

--- main.cpp
+++ main.cpp
@@ -1,9 +1,8 @@
 #include "mainwindow.h"
 #include <QApplication>
-
 int main(int argc, char *argv[]) {
 	QApplication a(argc, argv);
-	MainWindow w(nullptr, argc > 1 ? argv[1] : "tobiipro.cfg");
+	MainWindow w(nullptr, argc > 1 ? argv[1] : nullptr);
 	w.show();
 	return a.exec();
 }
